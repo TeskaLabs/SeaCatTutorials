aa842708aeec1669000faa2d6e516a403d83848c
Radek Tomasek <radek.tomasek@gmail.com>
2014-08-30 18:04:38 +0200

    added POST handling and chunk limts

--- ch02_ios_simple_post/Host/NodeHostSimplePost.js
+++ ch02_ios_simple_post/Host/NodeHostSimplePost.js
@@ -7,17 +7,46 @@ Created in 2014 by Radek Tomasek / Seal Teaks Ltd.
 */
 
 var http = require('http');
+var querystring = require('querystring');
 
 http.createServer(function (request, response) {
-	// Check whether the method is POST
+	// Check whether the method is POST.
 	if (request.method == 'POST') 
 	{
-		// Redirect the output to STDOUT.
-		request.pipe(process.stdout);		
-		// Prepare a header.  
-		response.writeHead(200, {'Content-Type': 'text/json'});		
-		// Write the response.
-		request.pipe(response);		
+		// variable postData is for storing incoming chunks of POST data.
+		var postData = "";
+		
+		// set UTF-8 encoding for request.
+		request.setEncoding("utf-8");
+		
+		// processing of POST chunks. Active method until all chunks have been collected.
+		request.addListener('data', function(data){
+			// collect POST data.
+			postData += data;
+			// limit the size of buffer -> 1e6 = 1 * 10 ^ 6 = ~ 1 MB.
+			if (postData.length > 1e6)
+			{
+				// Kill the connection if too much of POST data.
+				request.connection.destroy();
+			}
+		});
+		
+		// once all POST data is collected, following method is triggered.
+		request.addListener('end', function(){
+			// Write the result to console. 
+			console.log(JSON.parse(postData));
+		});	
+		// Prepare the header.
+		response.writeHead(200, {'Content-Type': 'text/json'});					
+		// End the request.
+		response.end();
+	}
+	// Handle the request if method is something else than POST.
+	else 
+	{
+		response.writeHead(200, "Default request", {'Content-Type': 'text/plain'});
+		// End the request.
+		response.end();
 	}
 }).listen(1337);
 
