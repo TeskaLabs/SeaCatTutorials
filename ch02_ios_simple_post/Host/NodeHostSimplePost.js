613c52124ba077e05060d58d70202b1595175037
Radek Tomasek <radek.tomasek@gmail.com>
2014-09-02 18:41:28 +0200

    Host update

--- ch02_ios_simple_post/Host/NodeHostSimplePost.js
+++ ch02_ios_simple_post/Host/NodeHostSimplePost.js
@@ -10,20 +10,20 @@ var http = require('http');
 var querystring = require('querystring');
 
 http.createServer(function (request, response) {
-	// Check whether the method = 'POST' and content type = 'application/x-www-form-urlencoded'.
-	if (request.headers['content-type'] === 'application/x-www-form-urlencoded' && request.method === 'POST') 
+	// Check whether the method = 'POST.
+	if (request.method === 'POST') 
 	{
-		// variable postData is for storing incoming chunks of POST data.
+		// Variable postData is for storing incoming chunks of POST data.
 		var postData = "";
 		
-		// set UTF-8 encoding for request.
+		// Set UTF-8 encoding for request.
 		request.setEncoding("utf-8");
 		
-		// processing of POST chunks. Active method until all chunks have been collected.
+		// Processing of POST chunks. Active method until all chunks have been collected.
 		request.addListener('data', function(data){
-			// collect POST data.
+			// Collect POST data.
 			postData += data;
-			// limit the size of buffer -> 1e6 = 1 * 10 ^ 6 = ~ 1 MB.
+			// Limit the size of buffer -> 1e6 = 1 * 10 ^ 6 = ~ 1 MB.
 			if (postData.length > 1e6)
 			{
 				// Kill the connection if too much of POST data.
@@ -31,23 +31,28 @@ http.createServer(function (request, response) {
 			}
 		});
 		
-		// once all POST data is collected, following method is triggered.
+		// Once all POST data is collected, following method is triggered.
 		request.addListener('end', function(){
-			// Prepare the header.
-			response.writeHead(200, {'Content-Type': 'text/json'});		
-			// Read post data.
-			var post = querystring.parse(postData);
-			// Write the result to console. 
-			console.log(post);
-			// End the request.
-			response.end();
+			// Check if really received POST (based on the content type).
+			if (request.headers['content-type'] === 'application/x-www-form-urlencoded')
+			{
+				// Prepare the header.
+				response.writeHead(200, {'Content-Type': 'text/json'});		
+				// Read post data.
+				var post = querystring.parse(postData);
+				// Write the result to console. 
+				console.log(post);
+				// End the request.
+				response.end();
+			}
 		});
 	}
 	// Handle the request if method is something else than POST.
 	else 
 	{
+		// Write default header.
 		response.writeHead(200, "Default request", {'Content-Type': 'text/plain'});
-		// End the request.
+		// End of the Request.
 		response.end();
 	}
 }).listen(1337);
