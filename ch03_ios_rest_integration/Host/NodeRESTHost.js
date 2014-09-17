3b6866497bf42ed897bbefb6733bb374336a0f89
Radek Tomasek <radek.tomasek@gmail.com>
2014-09-17 23:46:28 +0200

    Updated basic structure of NodeRESTHost.js application

--- ch03_ios_rest_integration/Host/NodeRESTHost.js
+++ ch03_ios_rest_integration/Host/NodeRESTHost.js
@@ -5,4 +5,24 @@
 // 
 // Created in 2014 by Radek Tomasek / SeaCat Ltd. 
 
+// Include Express.js framework scripts.
 var express = require('express');
+
+// Initialize app by using Express framework.
+var app = express();
+
+// Set default port 1337 or custom if defined by user externally.
+app.set('port', process.env.PORT || 1337);
+
+
+
+
+
+
+
+
+
+// Start listening on defined port, this keep running the application until hit Ctrl + C key combination.  
+app.listen(app.get('port'), function(){
+  console.log("Host is running and listening on http://localhost:" + app.get('port') + '; press Ctrl-C to terminate.');  
+});
