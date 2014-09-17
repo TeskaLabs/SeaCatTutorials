f884176ea36432c3563283b9eef657cd09205341
Radek Tomasek <radek.tomasek@gmail.com>
2014-09-17 23:33:07 +0200

    ch03_ios_rest_integration initial commit; added Gateway configuration, initial js host file and .gitignore file, which help to avoid adding directory with external modules into GIT.

---
+++ ch03_ios_rest_integration/Host/NodeRESTHost.js
@@ -0,0 +1,8 @@
+// NodeRESTHost.js
+// A simple REST host in Node.js with using Express.js framework.
+//
+// All data stored in memory, requests are handled in particular routes.
+// 
+// Created in 2014 by Radek Tomasek / SeaCat Ltd. 
+
+var express = require('express');
