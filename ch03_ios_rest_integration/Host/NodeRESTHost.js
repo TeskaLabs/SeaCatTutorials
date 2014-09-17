132d08ccd74de9a4845913f516e7104d7c3e50c7
Radek Tomasek <radek.tomasek@gmail.com>
2014-09-17 23:55:45 +0200

    Added 404 and 500 error handlers

--- ch03_ios_rest_integration/Host/NodeRESTHost.js
+++ ch03_ios_rest_integration/Host/NodeRESTHost.js
@@ -20,7 +20,23 @@ app.set('port', process.env.PORT || 1337);
 
 
 
+// Use Express midleware to handle 404 and 500 error states.
+app.use(function(request, response){
+   // Set status 404 if none of above routes processed incoming request. 
+   response.status(404); 
+   // Generate the output
+   response.send('404 - not found');
+});
 
+// 500 error handling. This will be handled in case of any internal issue on the host side.
+app.use(function(err, request, response){
+  // Set response type to application/json.
+  response.type('application/json');
+  // Set response status to 500 (error code for internal server error).
+  response.status(500);
+  // Generate the output - an Internal server error message. 
+  response.send('500 - internal server error');
+});
 
 // Start listening on defined port, this keep running the application until hit Ctrl + C key combination.  
 app.listen(app.get('port'), function(){
