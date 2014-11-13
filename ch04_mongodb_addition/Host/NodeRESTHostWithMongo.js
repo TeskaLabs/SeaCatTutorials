ed84171a7b7342b6a2acf33e9eebe7005f8dd75a
Radek Tomasek <radek.tomasek@gmail.com>
2014-11-13 03:06:22 -0800

    MongoSB default value + some methods to read data

--- ch04_mongodb_addition/Host/NodeRESTHostWithMongo.js
+++ ch04_mongodb_addition/Host/NodeRESTHostWithMongo.js
@@ -5,9 +5,42 @@
 // 
 // Created in 2014 by Radek Tomasek / SeaCat Ltd. 
 
-// Include external modules - Express.js and BodyParser.
+// Include external modules - Express.js, BodyParser, Mongoose and autoincrement.
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
+var opts = {
+  server: {
+    socketOptions: {keepAlive: 1}
+  }
+};
+
+// Create Schema variable. 
+var Schema = mongoose.Schema;
+
+// Define local connection string with MongoDB.
+var connectionString = 'mongodb://localhost/mymovies';
+
+// Connect to local MongoDB. 
+var connection = mongoose.connect(connectionString, opts);
+
+// Use auto increment module.
+autoIncrement.initialize(connection);
+
+// Definition of the movie schema
+var movieSchema = new Schema({
+  name: String,
+  director: String,
+  release: Number
+});
+
+// Bind autoincrement plugin with schema.
+movieSchema.plugin(autoIncrement.plugin, 'Movie');
+
+// Prepare object for export.
+var Movie = connection.model('Movie', movieSchema);
 
 // Initialize app by using Express framework.
 var app = express();
@@ -19,85 +52,66 @@ app.use(bodyParser.json());
 // Set default port 1337 or custom if defined by user externally.
 app.set('port', process.env.PORT || 1337);
 
-// Initialization of movies array.
-var movies = [
-  {
-    id: 1,
+// Seeding Initial Data.
+Movie.find(function(err, movies){
+  // In case of any intial data already exists, skip the rest of the method.
+  if (movies.length) {
+    return;
+  }
+  
+  new Movie({
     name: "Forrest Gump",
     director: "Robert Zemeckis",
     release: 1994
-  },
-  { 
-    id: 2,
+  }).save();
+  
+  new Movie({
     name: "Donnie Darko",
     director: "Richard Kelly",
     release: 2001
-  },
-  {
-    id: 3,
+  }).save();
+  
+  new Movie({
     name: "Inception",
     director: "Christopher Nolan",
     release: 2010
-  }
-]; 
-
-// A simulation of creating new IDs. Basically get the last element and increase the value of an ID. 
-function getNewId(){
-  return movies[movies.length -1].id + 1;
-}
-
-// Function findIndexOfElement helps to identify the array index according to specified key/value pair. 
-function findIndexOfElement(inputArray, key, value){
-  for (var i = 0; i < inputArray.length; i++){
-    if (inputArray[i][key] === value){
-      return i;
-    }
-  }
-  return -1;
-}
+  }).save();
+});
 
 // GET - list of all records.
-app.get('/api/movies', function(request, response){
-  response.json(movies.map(function(movie){
-    return {
-      id: movie.id,
-      name: movie.name,
-      director: movie.director,
-      release: movie.release
+app.get('/api/movies', function(request, response, next){
+  Movie.find(function(error, movies){
+    // In case of any error, forward request to error handler.
+    if (error) {
+      next();
     }
-  }));
+    // List of all records from db.
+    response.json(movies.map(function(movie){
+      return {
+       id: movie._id,
+       name: movie.name,
+       director: movie.director,
+       release: movie.release
+      }
+    }));
+  });
 });
 
 // GET - list of a record with particular id. If not found, forward the request to 404 - not found. 
 app.get('/api/movies/:id', function(request, response, next){  
-  // Get an integer interpretation of URL parameter. 
-  var urlIntParam = parseInt(request.params.id);
-  // Check whether the element is a valid positive number. If not (following case, redirect the request to 404).
-  if (urlIntParam < 0 || isNaN(urlIntParam)){
-    // Use following middleware - matched 404.
-    next();
-  }
-  else {
-    // Find array index in our movie array based on the input parameter (converted to integer). 
-    var elementIndex = findIndexOfElement(movies, 'id', urlIntParam);
-    // If element exists, get the response, otherwise redirect to 404.
-    if (elementIndex >= 0){
-      // Get an object from movie array.
-      var selectedMovie = movies[elementIndex];
-      // Return JSON response with selected attributes.
-      response.json({
-        id: selectedMovie.id,
-        name: selectedMovie.name,
-        director: selectedMovie.director,
-        release: selectedMovie.release
-      });
+  Movie.findById(request.params.id, function(error, movie){
+    // In case of any error, forward request to error handler.
+    if (error) {
+      next();
     }
-    else
-    {
-      // redirection to 404.
-      next();      
-    }
-  }
+    // Otherwise render the content.
+    response.json({
+      id: movie._id,
+      name: movie.name,
+      director: movie.director,
+      release: movie.release
+    });
+  });
 });
 
 // POST - create a new element.
@@ -105,12 +119,21 @@ app.post('/api/movies', function(request, response){
   // Complete request body.
   var requestBody = request.body;
   
-  movies.push({
-    id: getNewId(),
+  // Prepare new data.
+  var movie = new Movie({
     name: requestBody.name,
     director: requestBody.director,
     release: requestBody.release
-  });
+  }); 
+  
+  movie.save(function(error, movie){
+    // In case of any error, forward request to error handler.
+    if (error) {
+      next();
+    }
+    
+    response.json({id: movie._id});
+  });  
   
   response.status(200).end();
 });
